9c29981d2e08886c35e6c2c99ad6edba327a64af
Avinash Tiwari <aviprf10@gmail.com>
2023-12-25 21:53:30 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const Pagination = () => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage] = useState(10);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
+      );
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+      alert('Failed to fetch data');
+    }
+  };
+
+  const indexOfLastEmployee = currentPage * perPage;
+  const indexOfFirstEmployee = indexOfLastEmployee - perPage;
+  const currentEmployees = data.slice(indexOfFirstEmployee, indexOfLastEmployee);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleNext = () => {
+    if (currentPage < Math.ceil(data.length / perPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Employee Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            {/* Add more columns as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {currentEmployees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.name}</td>
+              <td>{employee.email}</td>
+              {/* Add more columns as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={handlePrevious} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span> Page {currentPage} </span>
+        <button onClick={handleNext} disabled={currentPage === Math.ceil(data.length / perPage)}>
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Pagination;
