3ab63fa24c610ad4769ea8536b3015081d64b275
Avinash Tiwari <aviprf10@gmail.com>
2023-12-25 22:23:18 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,75 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom'; // Import the jest-dom library
+// src/App.js
+import React from 'react';
+import EmployeeList from './EmployeeList';
 
-const Pagination = () => {
-  const [data, setData] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [perPage] = useState(10);
-
-  useEffect(() => {
-    fetchData();
-  }, [currentPage]);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
-      );
-      const result = await response.json();
-      setData(result);
-    } catch (error) {
-      console.error('Failed to fetch data:', error);
-      alert('Failed to fetch data');
-    }
-  };
-
-  const indexOfLastEmployee = currentPage * perPage;
-  const indexOfFirstEmployee = indexOfLastEmployee - perPage;
-  const currentEmployees = data.slice(indexOfFirstEmployee, indexOfLastEmployee);
-
-  const handleNext = () => {
-    if (currentPage < Math.ceil(data.length / perPage)) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlePrevious = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
+function App() {
   return (
-    <div>
-      <h1>Employee Data</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentEmployees.map((employee) => (
-            <tr key={employee.id}>
-              <td>{employee.name}</td>
-              <td>{employee.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        <button onClick={handlePrevious} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <span> Page {currentPage} </span>
-        <button onClick={handleNext} disabled={currentPage === Math.ceil(data.length / perPage)}>
-          Next
-        </button>
-      </div>
+    <div className="App">
+      <h1>Employee Pagination</h1>
+      <EmployeeList />
     </div>
   );
-};
+}
 
-export default Pagination;
+export default App;
---
+++ src/EmployeeList.js
@@ -0,0 +1,103 @@
+// src/EmployeeList.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [employeesPerPage] = useState(10);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
+        );
+        setEmployees(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError('Failed to fetch data');
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Logic for pagination
+  const indexOfLastEmployee = currentPage * employeesPerPage;
+  const indexOfFirstEmployee = indexOfLastEmployee - employeesPerPage;
+  const currentEmployees = employees.slice(indexOfFirstEmployee, indexOfLastEmployee);
+
+  // Change page
+  const paginate = (pageNumber) => {
+    const totalPages = Math.ceil(employees.length / employeesPerPage);
+
+    if (pageNumber > 0 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+    }
+  };
+
+  // Display loading state
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  // Display error message
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            {/* Add additional table headers as needed */}
+          </tr>
+        </thead>
+        <tbody>
+          {currentEmployees.map((employee) => (
+            <tr key={employee.id}>
+              <td>{employee.id}</td>
+              <td>{employee.name}</td>
+              {/* Add additional table data as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* Pagination component */}
+      <div>
+        {/* Previous button */}
+        <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+          Previous
+        </button>
+
+        {/* Page numbers */}
+        {Array.from({ length: Math.ceil(employees.length / employeesPerPage) }).map(
+          (item, index) => (
+            <button key={index} onClick={() => paginate(index + 1)}>
+              {index + 1}
+            </button>
+          )
+        )}
+
+        {/* Next button */}
+        <button
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPage === Math.ceil(employees.length / employeesPerPage)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeList;
